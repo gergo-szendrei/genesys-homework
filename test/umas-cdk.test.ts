8585424f01bf5f7d3538386b26c1949f28292765
Gergo Szendrei <gergo.szendrei@intuitech.studio>
2024-12-06 11:54:02 +0100

    Initial commit

---
+++ test/umas-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as UmasCdk from '../lib/umas-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/umas-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new UmasCdk.UmasCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
